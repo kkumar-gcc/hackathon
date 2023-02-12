5b4c917cec61ff9b1fd54fd902f5f1709f0f5bb3
kkumar-gcc <krishkumar9352@gmail.com>
2023-02-12 22:23:18 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App p-4">
+      <h1>Life on land</h1>
+      <div className="container">
+        <img
+          src="https://images.unsplash.com/photo-1511884642898-4c92249e20b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFuZHNjYXBlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1400&q=60" />
+        <img
+          src="https://images.unsplash.com/photo-1434725039720-aaad6dd32dfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGFuZHNjYXBlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1400&q=60" />
+        <img
+          src="https://images.unsplash.com/34/BA1yLjNnQCI1yisIZGEi_2013-07-16_1922_IMG_9873.jpg?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGxhbmRzY2FwZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60" />
+      </div>
+      <h3>Top 10 Largest Land Animals
+      </h3>
+      <div className="container">
+        <img
+          src="https://images.unsplash.com/photo-1557050543-4d5f4e07ef46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlcGhhbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60" />
+        <img
+          src="https://images.unsplash.com/photo-1538127426967-75a6c73f6d20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Z2lyYWZmZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60" />
+        <img
+          src="https://images.unsplash.com/photo-1569979227661-459bbb0ebe84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b3N0cmljaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60" />
+        <img
+          src="https://images.unsplash.com/photo-1589656966895-2f33e7653819?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9sYXIlMjBiZWFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1400&q=60" />
+        <img
+          src="https://images.unsplash.com/photo-1614301970871-06870c281354?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNyb2NvZGlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60" />
+        <img src="https://www.animalsaroundtheglobe.com/wp-content/uploads/2022/03/anaconda-3778042_960_720.jpeg" />
+        <img
+          src="https://images.unsplash.com/photo-1598315805844-d15164294b16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhcHliYXJhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60" />
+        <img
+          src="https://images.unsplash.com/photo-1636011371935-bb36b26be0b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8d2hpdGUlMjByaGlub2Nlcm9zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60" />
+        <img
+          src="https://images.unsplash.com/photo-1619535211143-8e209a7b662c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGlwcG9wb3RhbXVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60" />
+        <img
+          src="https://images.unsplash.com/photo-1508605375977-9fe795aea86a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FwZSUyMGJ1ZmZhbG98ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60" />
+      </div>
+    </div>
+  )
+}
+
+export default App
